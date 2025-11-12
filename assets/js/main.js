286e3ffba138577d910a27d6966e03f2d115036f
Bryan Sanchez Garcia <bhyanxdev@gmail.com>
2025-11-12 10:27:43 -0500

    Modificación de nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,16 @@
 * License: https://bootstrapmade.com/license/
 */
 
+// Initialize Lenis
+const lenis = new Lenis({
+  autoRaf: true,
+});
+
+// Listen for the scroll event and log the event data
+lenis.on('scroll', (e) => {
+  console.log(e);
+});
+
 (function() {
   "use strict";
 
